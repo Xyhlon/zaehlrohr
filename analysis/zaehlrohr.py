f629fd87f2b6093dea902aeb3e2268e5de95e831
Maximilian Philipp <philipp@student.tugraz.at>
2022-11-15 23:52:30 +0100

    updates of the tables

--- analysis/zaehlrohr.py
+++ analysis/zaehlrohr.py
@@ -73,6 +73,7 @@ def test_zaehlrohr_protokoll():
         zAlu0_8,
         zAlu4,
         name="absorption_qal_raw",
+        inline_units=True,
     )
     # A2 Daten besorgen Untersuchung der Zählrohrcharakteristik Na22
     filepath = os.path.join(os.path.dirname(__file__), "../data/charakter.csv")
@@ -81,10 +82,10 @@ def test_zaehlrohr_protokoll():
     P.data["dz1"] = 0
     P.data["dz2"] = 0
     P.data["dz3"] = 0
-    P.print_table(U, z1, z2, z3, name="charakter_raw")
+    P.print_table(U, z1, z2, z3, name="charakter_raw", inline_units=True)
     P.data["z"] = P.data[["z1", "z2", "z3"]].mean(axis=1)
     P.data["dz"] = P.data[["z1", "z2", "z3"]].sem(axis=1)
-    P.print_table(z, name="charakter_z_bar")
+    P.print_table(z, name="charakter_z_bar", inline_units=True)
 
     P.plot_data(
         ax,
@@ -105,7 +106,7 @@ def test_zaehlrohr_protokoll():
     P.data["dt"] = 0
     P.data["dn"] = 0
 
-    P.print_table(t, n, name="statistik_raw")
+    P.print_table(t, n, name="statistik_raw", split=True, inline_units=True)
     bins = (int)(n.data.max() - n.data.min()) // 10
     P.histoofseries(n.data, bins=bins, name="statistik")
     bins = (int)(n.data.max() - n.data.min()) // 5
@@ -118,10 +119,10 @@ def test_zaehlrohr_protokoll():
     P.data["dz1"] = 0
     P.data["dz2"] = 0
     P.data["dz3"] = 0
-    P.print_table(l, z1, z2, z3, name="abstand_raw")
+    P.print_table(l, z1, z2, z3, name="abstand_raw", inline_units=True)
     P.data["z"] = P.data[["z1", "z2", "z3"]].mean(axis=1)
     P.data["dz"] = P.data[["z1", "z2", "z3"]].sem(axis=1)
-    P.print_table(z, name="abstand_z_bar")
+    P.print_table(z, name="abstand_z_bar", inline_units=True)
     P.plot_data(
         ax,
         l,
@@ -146,7 +147,7 @@ def test_zaehlrohr_protokoll():
     P.data["dn"] = 4
     P.data["r"] = 0.05
     P.data["dr"] = 0.003
-    P.print_table(B, n, name="magneto_raw")
+    P.print_table(B, n, name="magneto_raw", inline_units=True)
     m_0 = 0.511  # MeV
     p = 299.792456 * B / 1000 * r  # radius
     E = m_0 * (((p / m_0) ** 2 + 1) ** 0.5 - 1)
@@ -156,7 +157,7 @@ def test_zaehlrohr_protokoll():
     P.resolve(p)
     print(p.data)
     print(E.data)
-    P.print_table(E, p, name="magneto_E_p")
+    P.print_table(E, p, name="magneto_E_p", inline_units=True)
     P.plot_data(
         ax,
         E,
